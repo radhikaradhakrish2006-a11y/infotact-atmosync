56fa6d17ae98f9217823739c3098353c35aeea10
Radhika <radhikaradhakrish2006@gmail.com>
2026-07-17 18:54:44 +0530

    Generated container IDs.Assigned unique IDs for containers

--- producer.py
+++ producer.py
@@ -8,8 +8,7 @@ producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
-
-# 12 Container IDs
+# 12 Container IDs 
 container_ids = [
     "CNT-1001",
     "CNT-1002",
